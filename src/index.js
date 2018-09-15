ac8a22c03ca8edfbef158f5a4138b5f365ccbb90
aleksei.bulgak <a.bulgak@intetics.com>
2018-09-15 18:12:49 +0300

    feature: code refatoring. Moved logic for operation append into separate method

--- src/index.js
+++ src/index.js
@@ -1,96 +1,47 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
     this.stack = [initialValue];
-    this.lowPriorityOperations = ["+", "-"];
-    this.middleLevelPriorityOperations = ["*", "/"];
-    this.highPriorityOperations = ["^"];
-    this.operations = this.lowPriorityOperations.concat(this.middleLevelPriorityOperations).concat(this.highPriorityOperations)
-    this.operationsFunctions = {
-      "+": (f, s) => f + s,
-      "-": (f, s) => f - s,
-      "*": (f, s) => f * s,
-      "/": (f, s) => f / s,
-      "^": (f, s) => Math.pow(f, s)
+    this.operations = {
+      "+": {priority: 1, func: (f, s) => f + s},
+      "-": {priority: 1, func: (f, s) => f - s},
+      "*": {priority: 2, func: (f, s) => f * s},
+      "/": {priority: 2, func: (f, s) => f / s},
+      "^": {priority: 3, func: (f, s) => Math.pow(f, s)}
     }
   }
 
-  add(number) {
-    // your implementation
-    this.stack.push(number)
-    this.stack.push("+")
-    return this;
+  _isPriorityHeigher(operation, operationInStack){
+    return this.operations[operation].priority >= this.operations[operationInStack].priority
   }
 
-  subtract(number) {
-    // your implementation
-    this.stack.push(number)
-    this.stack.push("-")
-    return this;
-  }
-
-  multiply(number) {
-    // your implementation
+  _appendOperation(operation, number){
     var lastIndex = this.stack.length - 1;
-    if (!this.lowPriorityOperations.includes(this.stack[lastIndex]) || Number.isInteger(this.stack[lastIndex])) {
+    var lastElement = this.stack[lastIndex];
+    if(Number.isInteger(lastElement) || !this._isPriorityHeigher(operation, lastElement)){
       this.stack.push(number)
-      this.stack.push("*")
+      this.stack.push(operation)
     } else {
       var tmpArray = [];
-      while (!Number.isInteger(this.stack[lastIndex]) && this.lowPriorityOperations.includes(this.stack[lastIndex])) {
+      while (!Number.isInteger(this.stack[lastIndex]) && this._isPriorityHeigher(operation, lastElement)) {
         tmpArray.push(this.stack.pop())
         lastIndex -= 1;
+        lastElement = this.stack[lastIndex];
       }
       this.stack.push(number)
-      this.stack.push("*")
+      this.stack.push(operation)
       this.stack = this.stack.concat(tmpArray.reverse());
     }
-    return this;
   }
 
-  devide(number) {
-    // your implementation
-    var lastIndex = this.stack.length - 1;
-    if (!this.lowPriorityOperations.includes(this.stack[lastIndex]) || Number.isInteger(this.stack[lastIndex])) {
-      this.stack.push(number)
-      this.stack.push("/")
-    } else {
-      var tmpArray = [];
-      while (!Number.isInteger(this.stack[lastIndex]) && this.lowPriorityOperations.includes(this.stack[lastIndex])) {
-        tmpArray.push(this.stack.pop())
-        lastIndex -= 1;
-      }
-      this.stack.push(number)
-      this.stack.push("/")
-      this.stack = this.stack.concat(tmpArray.reverse());
-    }
-
-    return this;
-  }
-
-  pow(number) {
-    // your implementation
-    var lastIndex = this.stack.length - 1;
-    var tmpArray = [];
-    while (!Number.isInteger(this.stack[lastIndex])) {
-      tmpArray.push(this.stack.pop())
-      lastIndex -= 1;
-    }
-    this.stack.push(number)
-    this.stack.push("^")
-    this.stack = this.stack.concat(tmpArray.reverse());
-    return this;
-  }
-
-  toString() {
+  _calculate(){
     var calculationStack = [this.stack.shift()];
     while (this.stack.length > 0 || calculationStack.length > 1) {
       let value = calculationStack[calculationStack.length - 1];
-      if (this.operations.includes(value)) {
+      if (this.operations.hasOwnProperty(value)) {
         let operation = calculationStack.pop();
         let second = calculationStack.pop();
         let first = calculationStack.pop();
-        calculationStack.push(this.operationsFunctions[operation](first, second));
+        calculationStack.push(this.operations[operation].func(first, second));
       }
       if (this.stack.length > 0) {
         calculationStack.push(this.stack.shift());
@@ -98,6 +49,45 @@ class SmartCalculator {
     }
     return calculationStack.pop();
   }
+
+  add(number) {
+    this.stack.push(number)
+    this.stack.push("+")
+    return this;
+  }
+
+  subtract(number) {
+    this.stack.push(number)
+    this.stack.push("-")
+    return this;
+  }
+
+  multiply(number) {
+    this._appendOperation("*", number)
+    return this;
+  }
+
+  devide(number) {
+    this._appendOperation("/", number)
+    return this;
+  }
+
+  pow(number) {
+    this._appendOperation("^", number)
+    return this;
+  }
+
+  toString() {
+    return this._calculate();
+  }
 }
 
 module.exports = SmartCalculator;
+new SmartCalculator(2).multiply(2)
+.pow(2)
+.subtract(95)
+.subtract(56)
+.pow(2)
+.pow(2)
+.pow(1)
+.multiply(1).toString()
